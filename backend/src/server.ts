3ff0c36e662411d00e0a2d01a7ae16f3c16aae2b
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2025-01-16 10:52:51 +0200

    add cors, authRouter

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import router from './routes/router';
+import cors from 'cors';
+
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/', router);
