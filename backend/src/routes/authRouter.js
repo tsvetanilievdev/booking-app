e4bb1e0c081229faa359b961b98407706a7ae633
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2025-01-16 22:50:18 +0200

    add protect middleware

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { protect } from "../middlewares/protect";
 
 const authRouter = Router();
+
+authRouter.use('/', protect);
 
 export default authRouter;
---
+++ backend/src/utils/authUtils.js
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const hashPassword = async (password) => bcrypt.hash(password, 10);
+export const comparePasswords = async (password, hashedPassword) => bcrypt.compare(password, hashedPassword);
+export const createToken = (user) => {
+    const token = jwt.sign({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+    }, process.env.JWT_SECRET)
+
+    return token;
+};
+export const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
